c490a73b37e6ff8778a0b869335da1ed34e3c1a5
unknown <mail.mrhishprung@gmail.com>
2023-02-27 14:23:21 +0100

    Added feat list and class choice with levels

--- creationPage.js
+++ creationPage.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict';   
     let attrVal = {
         STR: 10,
         DEX: 10,
@@ -25,3 +25,42 @@
             getElementById(`AttVal${modifiedAttr}`).
             innerHTML = attValues[`${modifiedAttr}`]
     };
+
+    function generateFeats()
+    {
+        function createPayload() {
+            let html = '';
+            let featList = pullFeatData();
+            console.log(featList)
+            let sourceImageLink = 'https://static.wikia.nocookie.net/nwn/images/9/94/Ife_bardsong.gif/revision/latest?cb=20050813220720';
+            let i;
+            for(i=0; i< featList.length; i++)
+            {
+                html += `<tr>
+                    <td>
+                        <input type="checkbox" id="featCheck${i}">
+                    </td>
+                    <td>
+                        <img src='${sourceImageLink}' id="featIcon">
+                    </td>
+                    <td id="featName">${featList[i]}</td>
+                </tr>`
+               
+                
+            }
+
+            return html;
+        }
+        
+        function pullFeatData()
+        {
+            //tempArray = JSON.parse('../featData.json')
+            let tempArray = ['test1', 'test2', 'test3'];
+            return tempArray;
+        }
+        let htmlResult = createPayload();
+        console.log(htmlResult)
+        document.querySelector('#featListTarget').innerHTML = htmlResult;
+        
+    }
+    generateFeats();
